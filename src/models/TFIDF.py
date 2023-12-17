6ad45541dc4316a2fd4a756dfdfdeff02e1787ac
kapiblue <kapiblue@outlook.com>
2023-12-17 21:41:03 +0100

    upload working vectorizer

--- src/models/TFIDF.py
+++ src/models/TFIDF.py
@@ -22,25 +22,31 @@ class Vectorizer():
     def loadTFIDFrecipes(self, tfidfRecipesPath):
         self.tfidf_recipes = pd.read_csv(tfidfRecipesPath,
                                          index_col=0)
+        print(self.tfidf_recipes.shape)
 
     def predict(self, inputSet, nBestMatches=3):
-        transformed = self.vectorizer.transform([" ".join(inputSet)]).toarray()
+        transformed = self.vectorizer.transform([" ".join(inputSet)])
+        print(transformed)
+        self.vectorizer.transform([" ".join(inputSet)]).toarray()
+        print(transformed.shape)
         similarities = self.similarity_f(transformed,
                                          self.tfidf_recipes).flatten()
         sorted_idx = np.argsort(similarities)[::-1].tolist()[:nBestMatches]
-        return [self.dataset["directions"].iloc[i] for i in sorted_idx]
+        print([self.dataset["title"].iloc[i] for i in sorted_idx])
+        return [self.dataset["ingredients"].iloc[i] for i in sorted_idx]
 
 
 if __name__ == "__main__":
-    datasetPath = "../../data/processed/dataset.csv"
+    datasetPath = "../../data/processed/modified_dataset.csv"
     vectorizerPath = "../../models/tfidf_vectorizer.pkl"
     tfidfRecipesPath = "../../data/processed/tfidf_recipes.csv"
 
     model = Vectorizer(datasetPath, vectorizerPath,
                        tfidfRecipesPath, cosine_similarity)
 
-    inputSet = ['eggs', 'pepper', 'salt', 'pork_sausage']
+    inputSet = ['eggs', 'pepper', 'salt',
+                'flour', 'milk', 'sugar']
 
-    results = model.predict(inputSet)
+    results = model.predict(inputSet, nBestMatches=10)
     for recipe in results:
         print(recipe)
