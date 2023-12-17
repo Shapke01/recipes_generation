32117197a45e83fb8fe5bb58d705e68866e94457
Shapke01 <stanislaw.hapke@gmail.com>
2023-12-17 20:28:50 +0100

    add overlap coefficient

--- src/features/metrics.py
+++ src/features/metrics.py
@@ -1,4 +1,5 @@
 def IoU(l1, l2):
+
     t1 = tuple(l1)
     t2 = tuple(l2)
 
@@ -6,3 +7,16 @@ def IoU(l1, l2):
     s2 = set(t2)
 
     return len(s1.intersection(s2)) / len(s1.union(s2))
+
+def OverlapCoefficient(l1, l2):
+
+    t1 = tuple(l1)
+    t2 = tuple(l2)
+
+    s1 = set(t1)
+    s2 = set(t2)
+
+    if len(s1) ==0 or len(s2) == 0:
+        return 0
+
+    return len(s1.intersection(s2)) / min(len(s1), len(s2))
